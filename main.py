83f5ded47ea51418a8e1f5877d29eadfc08de1b1
MrRobot-0-1 <bnikiita2007@gmail.com>
2025-01-30 23:31:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 from pprint import pprint
 from config import open_weather_token
+from background import keep_alive
 
 
 def get_weather(city, open_weather_token):
@@ -57,6 +58,6 @@ def main():
     city = input("Введите город: ")
     get_weather(city, open_weather_token)
 
-
+keep_alive()
 if __name__ == '__main__':
     main()
